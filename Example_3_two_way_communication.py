cafda7c0af1cfdec17ede9a924e0b7b3b9e9fcab
oguz_araz <raraz15@ku.edu.tr>
2022-03-16 13:54:33 +0100

    format

--- Example_3_two_way_communication.py
+++ Example_3_two_way_communication.py
@@ -6,20 +6,19 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
 
+# connection parameters
+ip = "127.0.0.1"
+receiving_from_pd_port = 1415
+sending_to_pd_port = 1123
+
+drum_voice_pitch_map = {"kick": 36, 'snare': 38, 'tom-1': 47, 'tom-2': 42, 'chat': 64, 'ohat': 63}
+drum_voices = list(drum_voice_pitch_map.keys())
 
 if __name__ == '__main__':
 
     # Lists for storing received values
     velocity = [0]
     quitFlag = [False]
-
-    # ------------------ OSC ips / ports ------------------ #
-    # connection parameters
-    ip = "127.0.0.1"
-    receiving_from_pd_port = 1415
-    sending_to_pd_port = 1123
-
-    # ----------------------------------------------------------
 
     # ------------------ OSC Receiver from Pd ------------------ #
     # create an instance of the osc_sender class above
@@ -54,12 +53,9 @@ if __name__ == '__main__':
     # ---------------------------------------------------------- #
 
     # ------------------ NOTE GENERATION  ------------------ #
-    drum_voice_pitch_map = {"kick": 36, 'snare': 38, 'tom-1': 47, 'tom-2': 42, 'chat': 64, 'ohat': 63}
-    drum_voices = list(drum_voice_pitch_map.keys())
-
+    
     while (quitFlag[0] is False):
         server.handle_request()
-        print(f"Received velocity value {velocity[0]}")
 
         # 1. generate a random pitched note with the provided received value
         pitch = int(random.randrange(40, 52))       # 1 octave
